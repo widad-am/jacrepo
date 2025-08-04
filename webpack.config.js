dbc4b5b4e94fc01ebdc1e262bdf6ee00f4bd7a8c
widad-am <widad.amraniacs@gmail.com>
2025-08-04 12:15:18 +0100

    chore: add mode production to webpack config, fix build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',  // ← ensures default optimizations and silences the warning
   entry: './src/js/main.js',
   output: {
-    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    path: path.resolve(__dirname, 'public/assets/js'),
+    filename: 'main.js',
     publicPath: '/assets/js/',
-    filename: 'main.js'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       },
       {
         test: /\.(woff2?|ttf|otf|eot)$/,
         type: 'asset/resource',
         generator: {
-          filename: path.join('../fonts', '[name][ext]')
-        }
-      }
-    ]
-  }
+          filename: path.join('../fonts', '[name][ext]'),
+        },
+      },
+    ],
+  },
 };
